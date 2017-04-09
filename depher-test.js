eff336fda1bc1af9ecec9d08556a277a40f4775b
volkovasystems <richeve.bebedor@gmail.com>
2017-04-09 08:26:17 +0800

    Updated dependency.

--- depher-test.js
+++ depher-test.js
@@ -4,3 +4,5 @@ const depher = require( "./depher.js" );
 
 console.log( depher( [ 1, 2, 3 ], NUMBER, false ) );
 console.log( depher( [ 1, 2, 3 ], STRING, null, "hello" ) );
+
+console.log( depher( [ 1,2,3 ], Array, "", 123, "yeah", [1,2,3] ) );
